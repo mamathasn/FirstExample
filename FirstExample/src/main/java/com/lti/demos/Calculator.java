ab5ec6486870b9f2d4913ef6056b5082cc2889ab
mamathasn <mamathasn99@gmail.com>
2022-03-14 17:01:08 +0530

    First commit

---
+++ FirstExample/src/main/java/com/lti/demos/Calculator.java
@@ -0,0 +1,14 @@
+package com.lti.demos;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		examplec c= new examplec();
+		int r=c.add(100, 200);
+		System.out.println(r);
+	}
+
+	
+
+}
---
+++ FirstExample/src/main/java/com/lti/demos/examplec.java
@@ -0,0 +1,11 @@
+package com.lti.demos;
+
+public class examplec {
+
+	public int add(int x, int y)
+    {
+        int result=x+y;
+        return result;
+    }
+
+}
